9255872d364e71e31d4e24fc0283861cc33446f8
Elvin Agaragimow <e.agaragimow@gmail.com>
2018-07-23 17:19:55 +0300

    I coded planning and history pages

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,9 +1,14 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 @Component({
   selector: 'mel-system',
   templateUrl: './system.component.html',
   styleUrls: ['./system.component.scss']
 })
-export class SystemComponent {
+export class SystemComponent implements OnInit {
+  time = new Date();
+
+  ngOnInit() {
+    setInterval(() => {this.time = new Date()}, 1000);
+  }
 }
