ca023fcb4e71c85a5cd0e34e6e8b019f12255168
Elvin Agaragimow <e.agaragimow@gmail.com>
2018-08-08 19:36:30 +0300

    write template and component for editCategoryComponent, and update RecordsPageComponent and template, CategoriesService added a methods: getCategories and updateCategory, css fixes

--- src/app/system/records-page/add-category/add-category.component.ts
+++ src/app/system/records-page/add-category/add-category.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {CategoriesService} from "../../shared/services/categories.service";
 import {Category} from "../../shared/models/category.model";
@@ -8,12 +8,11 @@ import {Category} from "../../shared/models/category.model";
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.scss']
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent {
+
+  @Output() onCategoryAdd = new EventEmitter<Category>();
 
   constructor(private categoriesService: CategoriesService) {
-  }
-
-  ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
@@ -23,8 +22,10 @@ export class AddCategoryComponent implements OnInit {
     const category = new Category(name, capacity);
 
     this.categoriesService.addCategory(category)
-      .subscribe((category: Category) => {
-        console.log(category);
+      .subscribe((c: Category) => {
+        form.reset();
+        form.form.patchValue({capacity: 1});
+        this.onCategoryAdd.emit(c);
       });
   }
 
