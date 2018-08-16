91f707ea255ad551b59ad200074c4d7fa2dfd38f
Elvin Agaragimow <e.agaragimow@gmail.com>
2018-08-16 13:01:58 +0300

    validated add-event's form, update event model, create method onSubmit for add-event component

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Category} from "../../shared/models/category.model";
+import {NgForm} from "@angular/forms";
+import {MELEvent} from "../../shared/models/event.model";
+import * as moment from 'moment';
 
 @Component({
   selector: 'mel-add-event',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  @Input() categories: Category[] = [];
+  types = [
+    {type: 'income', label: 'доход'},
+    {type: 'outcome', label: 'расход'}
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let {amount, description, category, type} = form.value;
+    if (amount < 0) amount *= -1;
+    let date = new Date();
+
+    const event = new MELEvent(
+      type, amount, +category,
+      moment().format('DD.MM.YYYY HH.mm.ss'), description
+    );
   }
 
 }
